ac36759b544ec7d4a706b88e5bfd7f89783cbf8f
Tony ID <tony.inuma@gmail.com>
2019-08-14 19:08:46 -0500

    Add: Data Sensor LM35

--- counter/counter.ino
+++ counter/counter.ino
@@ -1,11 +1,20 @@
 
-int counter = 0;
+//int counter = 0;
+float tempC;
+int pinLM35 = 0;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print(++counter, DEC);
-  delay(2000);
+  //Serial.print(++counter, DEC);
+  //delay(2000);
+
+  tempC = analogRead(pinLM35);
+
+  tempC = (5.0 * tempC * 100.0)/1024.0;
+  Serial.print(tempC);
+  Serial.print("\n");
+  delay(1000);
 }
